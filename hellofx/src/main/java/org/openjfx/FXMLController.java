2f1199770ba9d16a2e35440fc371cbaf326699ca
Bjørnar Hoff <bjornar_hoff@hotmail.no>
2019-03-26 15:32:33 +0100

    Endring

--- hellofx/src/main/java/org/openjfx/FXMLController.java
+++ hellofx/src/main/java/org/openjfx/FXMLController.java
@@ -11,8 +11,8 @@ public class FXMLController {
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        System.out.println("Steroids!");
+        label.setText("Hello BoleCato!");
     }
 
     public void initialize() {
