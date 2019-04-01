9985fd8950fb534d8e224d683e93d3e868822205
Bjørnar Hoff <bjornar_hoff@hotmail.no>
2019-04-01 13:04:25 +0200

    FXML

--- hellofx/src/main/java/org/openjfx/FXMLController.java
+++ hellofx/src/main/java/org/openjfx/FXMLController.java
@@ -2,17 +2,37 @@ package org.openjfx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class FXMLController {
 
-    @FXML
-    private Label label;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("Steroids!");
-        label.setText("!");
+    private Button create;
+
+    @FXML
+    private Label kunde;
+
+    @FXML
+    private Label forsikringsNr;
+
+    @FXML
+    private Button generateF;
+
+
+    @FXML
+    private void createCustomer (ActionEvent event) {
+        System.out.println("Kunde opprettet!");
+        kunde.setText("Ny kunde lagt til");
+
+    }
+
+    @FXML
+    private void createInsuranceNr (ActionEvent event) {
+        System.out.println("Forsikringsnummer generert");
+        forsikringsNr.setText("Forsikringsnummer er: ");
+
     }
 
     public void initialize() {
