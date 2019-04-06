b39e50fcbddf69e349359599cc16ef1d13d2212c
Sven <sven4696@gmail.com>
2019-04-06 15:07:48 +0200

    Laget Gui for Damage Report
    Laget Gui for damage report med fxml fil men har ikke koted noe funksjonalitet til det.

---
+++ hellofx/src/main/java/org/openjfx/DamageReportController.java
@@ -0,0 +1,15 @@
+package org.openjfx;
+
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextArea;
+
+
+public class DamageReportController {
+
+    @FXML
+    private TextArea title;
+
+
+}
