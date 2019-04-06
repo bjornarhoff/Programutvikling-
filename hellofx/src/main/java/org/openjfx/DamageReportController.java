c02983143f03018ce19f142a160ed8f685ab8999
Sven <sven4696@gmail.com>
2019-04-06 18:10:32 +0200

    Endret litt på Damage Report
    lagd ID til buttons og stylet litt anderledes.

--- hellofx/src/main/java/org/openjfx/DamageReportController.java
+++ hellofx/src/main/java/org/openjfx/DamageReportController.java
@@ -1,15 +1,27 @@
 package org.openjfx;
 
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.TextArea;
 
 
+
+
 public class DamageReportController {
 
     @FXML
-    private TextArea title;
+    private TextArea text_area;
+
+    @FXML
+    private JFXButton btn_create, btn_edit, btn_showDescription, btn_delete, btn_goBack;
 
 
+    @FXML
+    private void handleButtonActions(ActionEvent event){
+
+
+    }
 }
