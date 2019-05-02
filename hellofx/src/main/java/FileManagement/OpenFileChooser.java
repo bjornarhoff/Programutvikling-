0464b367627ee645616321c759d0a7826652708a
Cato Akay <catohilmi@hotmail.com>
2019-05-02 22:40:53 +0200

    Revert "Merge branch 'EksportMappe' into Cato"
    This reverts commit 57054ed7d36452e01f22e893c626ac405eb72aa2.

--- hellofx/src/main/java/FileManagement/OpenFileChooser.java
+++ hellofx/src/main/java/FileManagement/OpenFileChooser.java
@@ -73,30 +73,27 @@ public class OpenFileChooser {
         ArrayList<String> leisure_insurances = CsvReader.readAllLeisure();
         ArrayList<String> travel_insurances = CsvReader.readAllTravel();
 
-        File directory = new File (file.getPath().split("\\.")[0]);
-        if (!directory.exists()) {
-            directory.mkdir();
+        if (s.equals("csv")) {
+            String path = file.getPath().split("\\.")[0];
+            for (Customer customer : customers) {
+                CsvWriter.writeFileToCsvCustomer(file.getPath(), customer);
+            }
+            for (String boat_insurance : boat_insurances) {
+                CsvWriter.writeFileToCsv(path + "Boat.csv", boat_insurance);
+            }
+            for (String house_household_insurance : house_household_insurances) {
+                CsvWriter.writeFileToCsv(path + "House.csv", house_household_insurance);
+            }
+            for (String leisure_insurance : leisure_insurances) {
+                CsvWriter.writeFileToCsv(path + "Leisure.csv", leisure_insurance);
+            }
+            for (String travel_insurance : travel_insurances) {
+                CsvWriter.writeFileToCsv(path + "Travel.csv", travel_insurance);
+            }
+            for (String damage_report : damage_reports) {
+                CsvWriter.writeFileToCsv(path + "Damage.csv", damage_report);
+            }
         }
-            if (s.equals("csv")) {
-                String path = file.getPath().split("\\.")[0];
-                for (Customer customer : customers) {
-                    CsvWriter.writeFileToCsvCustomer(path + "/Customer.csv", customer);
-                }
-                for (String boat_insurance : boat_insurances) {
-                    CsvWriter.writeFileToCsv(path + "/Boat.csv", boat_insurance);
-                }
-                for (String house_household_insurance : house_household_insurances) {
-                    CsvWriter.writeFileToCsv(path + "/House.csv", house_household_insurance);
-                }
-                for (String leisure_insurance : leisure_insurances) {
-                    CsvWriter.writeFileToCsv(path + "/Leisure.csv", leisure_insurance);
-                }
-                for (String travel_insurance : travel_insurances) {
-                    CsvWriter.writeFileToCsv(path + "/Travel.csv", travel_insurance);
-                }
-                for (String damage_report : damage_reports) {
-                    CsvWriter.writeFileToCsv(path + "/Damage.csv", damage_report);
-                }
         if (s.equals("jobj")) {
             String path = file.getPath().split("\\.")[0];
 
